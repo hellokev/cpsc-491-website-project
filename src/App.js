374c18f15b2ea4688fa76cefe579a8d52be464bc
hellokev <2kevinloi8@gmail.com>
2023-02-23 21:44:23 -0800

    react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import twitter from './imgs/twitter-logo.png'
+import facebook from './imgs/facebook-logo.png'
+import reddit from './imgs/reddit-logo.png'
+import telegram from './imgs/telegram-logo.png'
+import discord from './imgs/discord-logo.png'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main-container">
+        <div className="top-container">
+          <p id="title">Asynchronous Applications</p>
+          <p id="project">Social Media scrapper</p>
+        </div>
+      </div>
+      <div className="social-media">
+        <img src={twitter} alt="twitter" className="social-logo" id="twitter"/>
+        <img src={facebook} alt="facebook" className="social-logo" id="facebook"/>
+        <img src={reddit} alt="reddit" className="social-logo" id="reddit"/>
+        <img src={telegram} alt="telegram" className="social-logo"id="telegram"/>
+        <img src={discord} alt="discord" className="social-logo" id="discord"/>
+      </div>
     </div>
   );
 }
